c933fc5f6bc71402872419b5ff6a577be20412e4
Dave <hector.moscoso@upb.edu.co>
2021-11-19 08:11:35 -0500

    Reubicación de los libros en cada puzzle y corrección de la transparencia en los posters

---
+++ Assets/Scripts/PuertaScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PuertaScript : MonoBehaviour
+{
+    [SerializeField] Image GBook;
+    [SerializeField] Image RBook;
+    [SerializeField] Image BBook;
+
+    [SerializeField] GameObject exitDoor;
+    [SerializeField] GameObject locker;
+    [SerializeField] AudioSource audiosrc;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
